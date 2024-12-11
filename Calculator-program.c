3263ae2a95aafceedc0e76d452ad4bb8b8d1daca
Thoibi <thoibiiheisname12@gmail.com>
2024-12-11 06:26:22 +0530

    message

---
+++ Calculator-program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number1, number2, sum;
+
+    // Asking the user to enter two integers
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // Calculating the sum
+    sum = number1 + number2;
